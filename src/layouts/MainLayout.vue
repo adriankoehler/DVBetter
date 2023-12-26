086de16a8861ed37627dd89b46c8a44a46aead95
Adrian Köhler <adrian.koehler@smk.sachsen.de>
2023-12-26 13:29:35 +0100

    remove logo and app title from header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title align="center">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          DVBetter
-        </q-toolbar-title>
-      </q-toolbar>
-
       <q-tabs align="justify">
         <q-route-tab to="/stations" label="Stations" />
         <q-route-tab to="/connections" label="Departures" />
