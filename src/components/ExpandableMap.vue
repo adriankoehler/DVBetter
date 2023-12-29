3bf129df37e541f331758dea9bd2849797793876
Adrian Köhler <adrian.koehler@smk.sachsen.de>
2023-12-29 17:11:31 +0100

    clean up expandable map component and adjust padding to be in line with page content

--- src/components/ExpandableMap.vue
+++ src/components/ExpandableMap.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="map-wrapper self-end shadow-2">
-        <v-divider></v-divider>
         <q-expansion-item
             expand-separator
             icon="map"
             expand-icon="expand_less"
             expanded-icon="expand_more"
-            label="Show on map"
         >
             <q-card>
-                <q-card-section>
+                <q-card-section class="q-px-none">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
                     commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
                     eveniet doloribus ullam aliquid.
@@ -25,6 +23,10 @@
 
 <style lang="scss">
 .map-wrapper {
-    width: 100%
+    width: 100%;
+
+    .q-item, .q-expansion-item__content {
+        padding: 0 2em;
+    }
 }
 </style>
