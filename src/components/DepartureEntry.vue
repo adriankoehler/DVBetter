bcd39f97496fc815bf4b5fdfa871ed30777cb933
Adrian Köhler <adrian.koehler@smk.sachsen.de>
2024-01-05 23:26:55 +0100

    add further styling for departure entry component

--- src/components/DepartureEntry.vue
+++ src/components/DepartureEntry.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-expansion-item header-class="departure-entry" expand-icon-class="expand-departure-icon">
+  <q-expansion-item header-class="departure-entry-header" expand-icon-class="expand-departure-icon">
     <template v-slot:header>
       <!-- icons for mode of transportation possible : directions_bus,tram,train -->
       <q-item-section avatar> {{ line }}              </q-item-section>
@@ -7,7 +7,7 @@
       <q-item-section side>   {{ arrivalTimeString }} </q-item-section>
     </template>
 
-    <q-card class="q-px-std">
+    <q-card class="q-px-std q-py-sm departure-entry-content">
       (some additional info)
     </q-card>
   </q-expansion-item>
@@ -30,12 +30,21 @@ const arrivalTimeString = dateFunctions.getArrivalTimeString(arrivalDateReal ? a
 </script>
 
 <style lang="scss">
-.departure-entry {
+.departure-entry-header {
   padding-left: $main-content-padding;
   padding-right: $main-content-padding;
 
+  .q-focus-helper {
+    display: none; //interferes with custom hover effect
+  }
+  &:hover {
+    background-color: $primary-10;
+    transition: background-color 0.4s ease-in-out;
+  }
+
   .q-item__section--avatar {
     font-weight: bold;
+    min-width: 45px;
   }
 
   .q-item__section--side {
@@ -45,12 +54,18 @@ const arrivalTimeString = dateFunctions.getArrivalTimeString(arrivalDateReal ? a
   .expand-departure-icon {
     color: $secondary;
   }
+}
 
-  // TODO
-  &.selected {
-    background: #DDA856;
-    color: white;
-    // wenn ausgeklappt noch opacity dazu
+.q-expansion-item--expanded {
+  .departure-entry-header {
+    background: $primary-10;
+    // background: #DDA85633;
+    color: $primary;
+    font-weight: bold;
+  }
+
+  .departure-entry-content {
+    background-color: $primary-05;
   }
 }
 </style>
