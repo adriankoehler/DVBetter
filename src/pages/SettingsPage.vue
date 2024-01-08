d896ddb64c0561463504720ff282bf5871e87136
Adrian Köhler <adrian.koehler@smk.sachsen.de>
2024-01-08 22:25:55 +0100

    add capacitor preferences API and add basic settings page as example

---
+++ src/pages/SettingsPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <div class="content-wrapper">
+      <h2>Settings</h2>
+      <q-btn @click="setObject">set</q-btn>
+      <q-btn @click="getObject">get</q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { Preferences } from '@capacitor/preferences';
+
+async function setObject() {
+  await Preferences.set({
+    key: 'user',
+    value: JSON.stringify({
+      id: 1,
+      name: 'Joseph'
+    })
+  })
+}
+
+async function getObject() {
+  const ret = await Preferences.get({ key: 'user' })
+  const user = JSON.parse(ret.value)
+  console.log(user)
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'settings', component: () => import('pages/SettingsPage.vue') },
       { path: 'connections/:connectionId?',
         children: [
           {
