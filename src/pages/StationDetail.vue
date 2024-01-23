91fb5b00258f516c979ab5d025e13c9537a97428
Adrian Köhler <adrian.koehler@smk.sachsen.de>
2024-01-23 18:01:02 +0100

    add pull-to-refresh functionality to station details site

--- src/pages/StationDetail.vue
+++ src/pages/StationDetail.vue
@@ -1,12 +1,18 @@
 <template>
   <q-page>
     <div class="content-wrapper no-x-padding">
-      <div class="row">
+      <q-pull-to-refresh @refresh="refresh">
+        <div class="row">
           <h2 class="q-pl-std">{{ stationName }}</h2>
-          <!-- bookmark und bookmark_border switchen -->
           <q-icon id="bookmark-station-icon" class="self-center cursor-pointer q-ml-auto q-pr-std" :name="icon" size="sm" @click="bookmark()"/>
-      </div>
-      <departure-entry v-for="departureEntry in departureData" :departure="departureEntry" :key="departureEntry.Id" />
+        </div>
+        <departure-entry
+          v-for="departureEntry in departureData"
+          :departure="departureEntry"
+          :key="departureEntry.Id + forceRefreshId"
+        />
+        <!-- other option to force refresh of departures:  :key="departureEntry.Id + (Math.floor(Date.now() / 6000))" -->
+      </q-pull-to-refresh>
     </div>
 
     <expandable-map :station="stationName"/>
@@ -41,6 +47,7 @@ const loading = ref(true)
 const departureData = ref(null)
 const stationName = ref("")
 const icon = ref("bookmark_border")
+let forceRefreshId = 0; //increased in refresh(); lets the component know that it should be reloaded (to update/recalculate the relative departure times)
 
 // offline testing
 // departureData.value = [ { "Id": "voe:11008: :H:j24", "DlId": "de:vvo:11-8", "LineName": "8", "Direction": "Südvorstadt", "Platform": { "Name": "4", "Type": "Platform" }, "Mot": "Tram", "RealTime": "/Date(1703804220000-0000)/", "ScheduledTime": "/Date(1703804280000-0000)/", "State": "InTime", "RouteChanges": [ "19653" ], "Diva": { "Number": "11008", "Network": "voe" }, "CancelReasons": [], "Occupancy": "Unknown" }, { "Id": "voe:21066: :R:j24", "DlId": "de:vvo:21-66", "LineName": "66", "Direction": "Nickern", "Platform": { "Name": "5", "Type": "Platform" }, "Mot": "CityBus", "RealTime": "/Date(1703804280000-0000)/", "ScheduledTime": "/Date(1703804280000-0000)/", "State": "InTime", "RouteChanges": [ "19653", "19471" ], "Diva": { "Number": "21066", "Network": "voe" }, "CancelReasons": [], "Occupancy": "Unknown" }, { "Id": "voe:11010: :H:j24", "DlId": "de:vvo:11-10", "LineName": "10", "Direction": "Gorbitz", "Platform": { "Name": "2", "Type": "Platform" }, "Mot": "Tram", "RealTime": "/Date(1703804340000-0000)/", "ScheduledTime": "/Date(1703804220000-0000)/", "State": "Delayed", "RouteChanges": [ "19446", "19653" ], "Diva": { "Number": "11010", "Network": "voe" }, "CancelReasons": [], "Occupancy": "Unknown" }, { "Id": "ddb:92D01: :R:j24", "DlId": "de:vvo:10-1", "LineName": "S1", "Direction": "Meißen S-Bf. Triebischtal", "Platform": { "Name": "14", "Type": "Railtrack" }, "Mot": "SuburbanRailway", "RealTime": "/Date(1703804400000-0000)/", "ScheduledTime": "/Date(1703804400000-0000)/", "State": "InTime", "RouteChanges": [ "19695", "19688" ], "Diva": { "Number": "92D01", "Network": "ddb" }, "CancelReasons": [], "Occupancy": "Unknown" }, { "Id": "voe:11003: :H:j24", "DlId": "de:vvo:11-3", "LineName": "3", "Direction": "Wilder Mann", "Platform": { "Name": "3", "Type": "Platform" }, "Mot": "Tram", "RealTime": "/Date(1703804700000-0000)/", "ScheduledTime": "/Date(1703804700000-0000)/", "State": "InTime", "RouteChanges": [ "19653" ], "Diva": { "Number": "11003", "Network": "voe" }, "CancelReasons": [], "Occupancy": "Unknown" } ]
@@ -50,11 +57,13 @@ const icon = ref("bookmark_border")
 const route = useRoute()
 const stationId = route.params.stationId //f.e.: Hbf=33000028
 
-if (stationId) {
-  settingsFunctions.isBookmarked(stationId).then(response => {
-    icon.value = response ? "bookmark" : "bookmark_border"
-  })
+function refresh(done) {
+  forceRefreshId++
+  fetchDepartures()
+  done()
+}
 
+function fetchDepartures() {
   api.post('/dm', {
           stopid: stationId,
           limit: 10,
@@ -89,6 +98,14 @@ if (stationId) {
           icon: 'report_problem'
         })
       })
+}
+
+if (stationId) {
+  settingsFunctions.isBookmarked(stationId).then(response => {
+    icon.value = response ? "bookmark" : "bookmark_border"
+  })
+
+  fetchDepartures()
 }
 
 async function bookmark() {
